0d6dc64f88cd487e95b28b30111b4466336a1173
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-06 11:30:33 +0400

    Created src dir and added pyproject.yaml

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="postmanager",
     version="0.1.0",
     description="Post manager for AWS S3 bucket storage",
     url="https://github.com/subaquatic-pierre/s3-postmanager",
     author="Pierre du Toit",
     author_email="https://pierredutoit.me/contact",
-    license="MIT",
-    url="http://pypi.python.org/pypi/postmanger/",
-    packages=["postmanager"],
-    long_description=open("README.md").read(),
+    project_urls={
+        "Bug Tracker": "https://github.com/subaquatic-pierre/s3-postmanager/issues",
+    },
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[
-        "boto3",
-    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
