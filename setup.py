dae76c3f8c321f82ec992484b01e9c2e457f53c2
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-10 10:06:31 +0400

    Update setup config

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="postmanager",
-    version="0.1.1",
-    description="Post manager for AWS S3 bucket storage",
-    url="https://github.com/subaquatic-pierre/postmanager",
-    author="Pierre du Toit",
-    author_email="subaquatic-pierre@gmail.com",
-    project_urls={
-        "Bug Tracker": "https://github.com/subaquatic-pierre/s3-postmanager/issues",
-    },
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    package_dir={"": "src"},
-    install_requires=["boto3"],
-    packages=setuptools.find_packages(where="src"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setup(setup_cfg=True)
