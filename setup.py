278c492a78d281b7e95e1bced5944e29fbe85cd5
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-18 11:23:06 +0400

    Updated setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r") as f:
-    readme = f.read()
 
-setup(
-    name="postmanager",
-    version="0.1.5",
-    description="Post manager to manage all post types with various storage backends",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    license_file="LICENSE",
-    author="Pierre du Toit",
-    author_email="subaquatic.pierre@gmail.com",
-    project_urls={
-        "Bug Tracker": "https://github.com/subaquatic-pierre/postmanager/issues"
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"src": "postmanager"},
-    packages=find_packages(exclude=["tests,site,docs"]),
-    test_suite="tests",
-    python_requires=">=3.6,<4",
-    install_requires=["boto3==1.21.38"],
-    entry_points={"console_scripts": ["postmanager=postmanager.cli:main"]},
-)
+setup()
